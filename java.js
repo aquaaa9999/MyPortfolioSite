90e19eadfe0ca45ffb7ada957d1daaa3a5c740c4
Alda Boyd <38465703+aquaaa9999@users.noreply.github.com>
2019-12-16 18:57:36 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+<script
+// Get the video
+var video = document.getElementById("myVideo");
+var audio = document.getElementById("myAudio1");
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
